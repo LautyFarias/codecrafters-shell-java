3e8ac4ba3e5e6e5a1c217c66fd82868005965b8b
Lautaro Farias <farias.lautaro002@gmail.com>
2024-08-06 16:26:19 -0300

    feat: The exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,20 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            System.out.printf("%s: command not found%n", input);
+            String[] tokens = input.split(" ");
+
+            switch (tokens[0]) {
+                case "exit":
+                    if (tokens.length > 1) {
+                        int exitCode = Integer.parseInt(tokens[1]);
+                        System.exit(exitCode);
+                    }
+
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.printf("%s: command not found%n", input);
+            }
         } while (true);
     }
 }
